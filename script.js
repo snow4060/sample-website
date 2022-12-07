3c3fa376a6452ae59d97b1dd3ffeb6d1e7a89606
snow4060 <luohaoyun4068@students.esuhsd.org>
2022-12-07 13:43:59 -0800

    organized the names and added some more

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-//import { allRandom } from 'human-names';
+// //import { allRandom } from 'human-names';
 
-document.body.onload = generateElements
+// document.body.onload = generateElements
 
-function generateElements(){
-    let amount = Math.floor(Math.random() * 100)   
-    for(let i = 0; i < amount; i++){
-        addElements()
-    } 
-}
+// function generateElements(){
+//     let amount = Math.floor(Math.random() * 100)   
+//     for(let i = 0; i < amount; i++){
+//         addElements()
+//     } 
+// }
 
 
-function addElements(){
-    if(Math.random() < 0.3){
-        const newDiv = document.createElement("b") //about 30% chance of bold
-        var str = names[Math.floor(Math.random()*100)]
-        const newContent = document.createTextNode(str)
-        newDiv.appendChild(newContent)
-        document.body.appendChild(newDiv)
-        document.body.appendChild(document.createElement("br"))
-        document.body.appendChild(document.createElement("br"))
-    }
-    else{
-        const newDiv = document.createElement("p")
-        var str = names[Math.floor(Math.random()*100)]
-        const newContent = document.createTextNode(str)
-        newDiv.appendChild(newContent)
-        document.body.appendChild(newDiv)
-    }
+// function addElements(){
+//     if(Math.random() < 0.3){
+//         const newDiv = document.createElement("b") //about 30% chance of bold
+//         var str = names[Math.floor(Math.random()*100)]
+//         const newContent = document.createTextNode(str)
+//         newDiv.appendChild(newContent)
+//         document.body.appendChild(newDiv)
+//         document.body.appendChild(document.createElement("br"))
+//         document.body.appendChild(document.createElement("br"))
+//     }
+//     else{
+//         const newDiv = document.createElement("p")
+//         var str = names[Math.floor(Math.random()*100)]
+//         const newContent = document.createTextNode(str)
+//         newDiv.appendChild(newContent)
+//         document.body.appendChild(newDiv)
+//     }
     
-}
+// }
 
-let names = ["James", "Robert", "John", "Michael", "David", "William", "Richard", "Joseph", "Thomas", "Charles", "Christopher", "Daniel", "Matthew", "Anthony", "Mark", "Donald", "Steven", "Paul", "Andrew", "Joshua", "Kenneth", "Kevin", "Brian", "George", "Timothy", "Ronald", "Edward", "Jason", "Jeffrey", "Ryan", "Jacob", "Gary", "Nicholas", "Eric", "Jonathan", "Stephan", "Larry", "Justin", "Scott", "Brandon", "Benjamin", "Samuel", "Gregory", "Alexander", "Frank", "Patrick", "Raymond", "Jack", "Dennis", "Jerry", "Emma", "Amelia", "Charlotte", "Ava", "Sophia", "Isabella", "Mia", "Evelyn", "Harper", "Luna", "Camilia", "Gianna", "Elizabeth", "Eleanor", "Ella", "Abigail", "Sofia", "Avery", "Scarlett", "Emily", "Aria", "Penelope", "Chloe", "Layla", "Mila", "Nora", "Hazel", "Madison", "Ellie", "Lily", "Isla", "Grace", "Violet", "Aurora", "Riley", "Zoey", "Willow", "Emilia", "Stella", "Zoe", "Victoria", "Hannah", "Addison", "Leah", "Lucy", "Eliana", "Ivy", "Everly"]
+// let names = ["James", "Robert", "John", "Michael", "David", "William", "Richard", "Joseph", "Thomas", "Charles", "Christopher", "Daniel", "Matthew", "Anthony", "Mark", "Donald", "Steven", "Paul", "Andrew", "Joshua", "Kenneth", "Kevin", "Brian", "George", "Timothy", "Ronald", "Edward", "Jason", "Jeffrey", "Ryan", "Jacob", "Gary", "Nicholas", "Eric", "Jonathan", "Stephan", "Larry", "Justin", "Scott", "Brandon", "Benjamin", "Samuel", "Gregory", "Alexander", "Frank", "Patrick", "Raymond", "Jack", "Dennis", "Jerry", "Emma", "Amelia", "Charlotte", "Ava", "Sophia", "Isabella", "Mia", "Evelyn", "Harper", "Luna", "Camilia", "Gianna", "Elizabeth", "Eleanor", "Ella", "Abigail", "Sofia", "Avery", "Scarlett", "Emily", "Aria", "Penelope", "Chloe", "Layla", "Mila", "Nora", "Hazel", "Madison", "Ellie", "Lily", "Isla", "Grace", "Violet", "Aurora", "Riley", "Zoey", "Willow", "Emilia", "Stella", "Zoe", "Victoria", "Hannah", "Addison", "Leah", "Lucy", "Eliana", "Ivy", "Everly"]
